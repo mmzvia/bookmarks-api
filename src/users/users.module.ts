2b5d0d669976ca01e7bb6149931cc39c5db3eda5
mikheili zviadadze <72224442+zviadadze@users.noreply.github.com>
2025-04-17 08:40:46 +0400

    fix: fix erros and update scripts

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { UsersController, UsersController2 } from './users.controller';
+import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
-  controllers: [UsersController, UsersController2],
+  controllers: [UsersController],
   providers: [UsersService],
 })
 export class UsersModule {}
